0a648af46615623a836767d3f3259fd8ae5c53d9
LauVii13 <viniciusglaureano@gmail.com>
2022-11-19 11:05:35 -0300

    Busca na API finalizada

--- GetAPI/GetAPI.ino
+++ GetAPI/GetAPI.ino
@@ -16,11 +16,11 @@ const char * ssid = "Honor";
 const char * password = "senha123"; 
 const char * host = "mylocker-api.herokuapp.com"; 
 String path = "/students"; // Path of Server
-const int httpsPort = 3333; // HTTPS PORT (default: 443)
+const int httpPort = 3333; // HTTPS PORT (default: 443)
 int refreshtime = 15; // Make new HTTPS request after x seconds
+WiFiServer server(80); 
 
-
-HTTPClient https;    //Declare object of class HTTPClient
+HTTPClient http;    //Declare object of class HTTPClient
 WiFiClient client;   // Declare out of loop like a global variable
 
 
@@ -33,15 +33,43 @@ void setup() {
   rfid.PCD_Init(); // Init MFRC522
   rfid.PCD_DumpVersionToSerial();
 
+
+
+        Serial.println(""); //PULA UMA LINHA NA JANELA SERIAL
+        Serial.println(""); //PULA UMA LINHA NA JANELA SERIAL
+        Serial.print("Conectando a "); //ESCREVE O TEXTO NA SERIAL
+        Serial.print(ssid); //ESCREVE O NOME DA REDE NA SERIAL
+         
+        WiFi.begin(ssid, password); //PASSA OS PARÂMETROS PARA A FUNÇÃO QUE VAI FAZER A CONEXÃO COM A REDE SEM FIO
+         
+        while (WiFi.status() != WL_CONNECTED) { //ENQUANTO STATUS FOR DIFERENTE DE CONECTADO
+        delay(500); //INTERVALO DE 500 MILISEGUNDOS
+        Serial.print("."); //ESCREVE O CARACTER NA SERIAL
+        }
+        Serial.println(""); //PULA UMA LINHA NA JANELA SERIAL
+        Serial.print("Conectado a rede sem fio "); //ESCREVE O TEXTO NA SERIAL
+        Serial.println(ssid); //ESCREVE O NOME DA REDE NA SERIAL
+        server.begin(); //INICIA O SERVIDOR PARA RECEBER DADOS NA PORTA DEFINIDA EM "WiFiServer server(porta);"
+        Serial.println("Servidor iniciado"); //ESCREVE O TEXTO NA SERIAL
+
+
+        Serial.print("IP para se conectar ao NodeMCU: "); //ESCREVE O TEXTO NA SERIAL
+        Serial.print("http://"); //ESCREVE O TEXTO NA SERIAL
+        Serial.println(WiFi.localIP()); //ESCREVE NA SERIAL O IP RECEBIDO DENTRO DA REDE SEM FIO (O IP NESSA PRÁTICA É RECEBIDO DE FORMA AUTOMÁTICA)
+
+       Serial.println(""); //PULA UMA LINHA NA JANELA SERIAL
+        Serial.println(""); //PULA UMA LINHA NA JANELA SERIAL
+
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
 }
 
-String ra = "200155";
+String ra = "";
 String cartao = "65 117 26 38";
 String leitor = "";
 String armario = "1";
+bool dono = false;
 
 void loop() {
 
@@ -70,17 +98,21 @@ void loop() {
 
   Serial.println("Leitor: " + leitor);
 
-  GetRequest("http://mylocker-api.herokuapp.com/students/" + ra + "/lockers/" + armario);
-
-  delay(500);
-  /*
+  
+  
+  
+  
     //verificacao se é dono ou n
     if(cartao == leitor){
-      Serial.println("Armario aberto");
+      ra = "200125";
     }
     else{
-      Serial.println("Armario nao liberado");
-    }*/
+      ra = "200076";
+    }
+
+      GetRequest("http://mylocker-api.herokuapp.com/students/" + ra + "/lockers/" + armario);
+
+  delay(500);
     
     rfid.PICC_HaltA();
           
@@ -116,38 +148,46 @@ void printDec(byte *buffer, byte bufferSize) {
 void GetRequest(String url){
   yield();
 
-  Serial.println(url);
-
-  if (http.begin(client, url)){
-    Serial.println("http.begin ok");
-  }
+    WiFiClient client;
+    HTTPClient http;
+    
+    Serial.println(url);
   
-  delay(100);
-  int httpCode = http.GET();
-  Serial.print("HTTP STATUS: ");
-  const String& payload = http.getString();
-  Serial.println(httpCode);
-  Serial.println(payload);
-
-  if(httpCode > 0){
-    switch (httpCode){
-      yield();
-      
-      case 200:
-          Serial.println("Valor correto(200)");
-        break;
-      
-      case 400:
-          Serial.println("Valor incorreto(400)");
-        break;
-      
-      default:
-          Serial.println("Nem um nem outro");
-        break;
+    if (http.begin(client, url)){
+      Serial.println("http.begin ok");
     }
-  }
-  else{
-    Serial.println("Fail Request");
-  }
+    
+    delay(100);
+    int httpCode = http.GET();
+    Serial.print("HTTP STATUS: ");
+    
+    Serial.println(httpCode);
 
+    if(httpCode > 0){
+      switch (httpCode){
+        yield();
+        
+        case 200:
+            Serial.println("Valor correto(200)");
+            dono = true;
+          break;
+        
+        case 400:
+            Serial.println("Valor incorreto(400)");
+            dono = false;
+          break;
+        
+        default:
+            Serial.println("Nem um nem outro");
+            dono = false;
+          break;
+      }
+    }
+    else{
+      Serial.println("Fail Request");
+    }
+    http.end();
+  
+  
 }
+
--- LeitorCartao/LeitorCartao.ino
+++ LeitorCartao/LeitorCartao.ino
@@ -1,5 +1,9 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
 
 #define SS_PIN D8
 #define RST_PIN D0
@@ -7,6 +11,18 @@
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
 MFRC522::MIFARE_Key key;
+
+const char * ssid = "Honor"; 
+const char * password = "senha123"; 
+const char * host = "mylocker-api.herokuapp.com"; 
+String path = "/students"; // Path of Server
+const int httpsPort = 3333; // HTTPS PORT (default: 443)
+int refreshtime = 15; // Make new HTTPS request after x seconds
+
+
+HTTPClient http;    //Declare object of class HTTPClient
+WiFiClient client;   // Declare out of loop like a global variable
+
 
 // Init array that will store new NUID
 byte nuidPICC[4];
@@ -22,12 +38,12 @@ void setup() {
   }
 }
 
+String ra = "200155";
 String cartao = "65 117 26 38";
 String leitor = "";
+String armario = "1";
 
 void loop() {
-
-  
 
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
@@ -36,42 +52,39 @@ void loop() {
     return;
 
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-
-              
-              for (byte i = 0; i < 4; i++) {
-                nuidPICC[i] = rfid.uid.uidByte[i];
-              }
-
-              leitor = "";
-              
-              Serial.println();
-             
-              Serial.print(F("In dec: "));
-
-              
-              
-              printDec(rfid.uid.uidByte, rfid.uid.size);
-              Serial.println();
-
-
-
-              Serial.println("Cartao: " + cartao);
-
-              Serial.println("Leitor: " + leitor);
-
-
-
-          //verificacao se é dono ou n
-              if(cartao == leitor){
-                Serial.println("Armario aberto");
-              }
-              else{
-                Serial.println("Armario nao liberado");
-              }
-    
-            rfid.PICC_HaltA();
           
-            rfid.PCD_StopCrypto1();
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+  }
+
+  leitor = "";
+              
+  Serial.println();
+             
+  Serial.print(F("In dec: "));
+          
+  printDec(rfid.uid.uidByte, rfid.uid.size);
+  Serial.println();
+
+  Serial.println("Cartao: " + cartao);
+
+  Serial.println("Leitor: " + leitor);
+
+  GetRequest("http://mylocker-api.herokuapp.com/students/" + ra + "/lockers/" + armario);
+
+  delay(500);
+  /*
+    //verificacao se é dono ou n
+    if(cartao == leitor){
+      Serial.println("Armario aberto");
+    }
+    else{
+      Serial.println("Armario nao liberado");
+    }*/
+    
+    rfid.PICC_HaltA();
+          
+    rfid.PCD_StopCrypto1();
 }
 
 void printDec(byte *buffer, byte bufferSize) {
@@ -97,6 +110,44 @@ void printDec(byte *buffer, byte bufferSize) {
         }
      }
         
-      
   }
+}
+
+void GetRequest(String url){
+  yield();
+
+  Serial.println(url);
+
+  if (http.begin(client, url)){
+    Serial.println("http.begin ok");
+  }
+  
+  delay(100);
+  int httpCode = http.GET();
+  Serial.print("HTTP STATUS: ");
+  const String& payload = http.getString();
+  Serial.println(httpCode);
+  Serial.println(payload);
+
+  if(httpCode > 0){
+    switch (httpCode){
+      yield();
+      
+      case 200:
+          Serial.println("Valor correto(200)");
+        break;
+      
+      case 400:
+          Serial.println("Valor incorreto(400)");
+        break;
+      
+      default:
+          Serial.println("Nem um nem outro");
+        break;
+    }
+  }
+  else{
+    Serial.println("Fail Request");
+  }
+
 }
